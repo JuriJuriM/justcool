b6315df9df42aff859d2f074b8f07f3fbdc2febb
JuriJuriM <jurimudry@gmail.com>
2025-02-13 10:45:08 +0300

    my first commit here

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="App">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
